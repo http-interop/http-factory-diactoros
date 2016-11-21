bb320678203feda65956ac358fe636d48028c363
Antonio J. García Lagar <aj@garcialagar.es>
2016-11-21 14:53:26 +0100

    Modify server request factory

--- src/ServerRequestFactory.php
+++ src/ServerRequestFactory.php
@@ -2,22 +2,30 @@
 
 namespace Http\Factory\Diactoros;
 
+use Http\Factory\Diactoros\UriFactory;
 use Interop\Http\Factory\ServerRequestFactoryInterface;
-use Interop\Http\Factory\ServerRequestFromGlobalsFactoryInterface;
-use Zend\Diactoros\ServerRequest;
-use Zend\Diactoros\ServerRequestFactory as Factory;
+use Psr\Http\Message\UriInterface;
+use Zend\Diactoros\ServerRequestFactory as DiactorosServerRequestFactory;
 
 class ServerRequestFactory implements
-    ServerRequestFactoryInterface,
-    ServerRequestFromGlobalsFactoryInterface
+    ServerRequestFactoryInterface
 {
-    public function createServerRequest($method, $uri)
+    public function createServerRequest(array $server, $method = null, $uri = null)
     {
-        return new ServerRequest([], [], $uri, $method);
-    }
+        $request = DiactorosServerRequestFactory::fromGlobals($server);
 
-    public function createServerRequestFromGlobals()
-    {
-        return Factory::fromGlobals();
+        if (null !== $method) {
+            $request = $request->withMethod($method);
+        }
+
+        if (null !== $uri) {
+            if (!$uri instanceof UriInterface) {
+                $uri = (new UriFactory())->createUri($uri);
+            }
+
+            $request = $request->withUri($uri);
+        }
+
+        return $request;
     }
 }
