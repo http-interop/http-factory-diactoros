aec9db2b8ef550e239988f0e01e01b793e1c9ae1
Antonio J. García Lagar <aj@garcialagar.es>
2016-11-21 14:49:52 +0100

    Add separate methods for creating streams

--- src/StreamFactory.php
+++ src/StreamFactory.php
@@ -7,7 +7,23 @@ use Zend\Diactoros\Stream;
 
 class StreamFactory implements StreamFactoryInterface
 {
-    public function createStream($resource)
+    public function createStream($content = '')
+    {
+        $resource = fopen('php://temp', 'r+');
+        fwrite($resource, $content);
+        rewind($resource);
+
+        return $this->createStreamFromResource($resource);
+    }
+
+    public function createStreamFromFile($file, $mode = 'r')
+    {
+        $resource = fopen($file, $mode);
+
+        return $this->createStreamFromResource($resource);
+    }
+
+    public function createStreamFromResource($resource)
     {
         return new Stream($resource);
     }
